aa0e185860532c1d73e7f4d10cad6c36b9f1c4b3
Roman Podymov <podymfrombryansk@yandex.com>
2024-09-10 10:33:25 +0200

    SPM (#4)

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let name = "PMKAddressBook"
+
+let pkg = Package(name: name)
+pkg.products = [
+    .library(name: name, targets: [name]),
+]
+pkg.swiftLanguageVersions = [.v4, .v4_2, .v5]
+pkg.dependencies = [
+	.package(url: "https://github.com/mxcl/PromiseKit.git", from: "8.1.2")
+]
+
+let target: Target = .target(name: name)
+target.path = "Sources"
+target.dependencies = ["PromiseKit"]
+
+let testTarget: Target = .testTarget(name: name + "Tests")
+testTarget.path = "Tests"
+target.dependencies = ["PMKAddressBook"]
+
+pkg.targets = [target, testTarget]
+
+pkg.platforms = [
+   .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)
+]
